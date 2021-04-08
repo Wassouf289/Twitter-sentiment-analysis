42918844d208cac454b6c5b67f1cbe75fb9fffce
Wassouf289 <mahmoudwassouf@gmail.com>
2021-04-08 15:48:05 +0200

    added config file for twitter API authentication

---
+++ tweet_collector/config.py
@@ -0,0 +1,5 @@
+API_KEY = ""              # provide your API key
+API_SECRET = ""   # provide your API secret
+ACCESS_TOKEN = ""  # provide your access token
+ACCESS_TOKEN_SECRET = " "  # provide your access token secret
+
